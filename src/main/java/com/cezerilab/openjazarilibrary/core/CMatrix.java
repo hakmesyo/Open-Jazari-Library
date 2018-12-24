f0bd29a2dfea509400f323bce8d86d02aa4cab5a
elcezerilab <elcezerilab@gmail.com>
2018-12-24 15:26:47 +0200

    all static jar were removed from the project for being pure maven project

--- src/main/java/com/cezerilab/openjazarilibrary/core/CMatrix.java
+++ src/main/java/com/cezerilab/openjazarilibrary/core/CMatrix.java
@@ -34,6 +34,17 @@
  * Save file
  * Restart/start netbeans.
  * https://blogs.oracle.com/tao/entry/set_netbeans_user_interface_language
+ * 
+ * 
+ * readme
+ *Open Jazari Library for Java (OJL4J) 
+ * Open Jazari Library for Java (OJL4J), formerly known as Open Cezeri Library is a Java based Matrix, Vectorization, Image Processing, Machine Learning (Weka + DL4J), Data Visualization and Analysis DSL (Domain Specific Language) which enables developers write just single line of code. It achieves this simplicity and ease of use by applying Fluent Interface and/or Method Chaining design pattern principle. Researchers, students, academicians and developers can code with their prior knowledge. Forexample, if he has already known Matlab methods such as plot, imshow he can easily find the similar methods in the OJL. It is also true for Python developer.
+
+You can refer this article and blog site for further information Ataş, Musa. "Open Cezeri Library: A novel java based matrix and computer vision framework." Computer Applications in Engineering Education (2016).
+
+https://open-cezeri-library.blogspot.com/
+
+https://www.youtube.com/watch?v=XiL8RtApi78
  *
  */
 package com.cezerilab.openjazarilibrary.core;
